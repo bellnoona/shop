f62d6b3bf124f2444c029469d0854040caf31eae
Soohyeon Kim <tngusglaso@gmail.com>
2022-07-21 23:11:04 +0900

    useEffect()

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,44 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 let YellowBtn = styled.button`
   background: ${(props) => props.bg};
-  color: ${(props) => (props.bg === 'skyblue' ? 'grey' : 'black')};
+  color: ${(props) => (props.bg === 'skyblue' ? 'gray' : 'black')};
   padding: 10px;
 `;
 
 function Detail(props) {
   let { id } = useParams();
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  }, [count]);
+
+  // for (var i = 0; i < 10000; i++) {
+  //   console.log(1);
+  // }
 
   return (
     <div className='container'>
-      <YellowBtn bg='skyblue'>버튼</YellowBtn>
-      <YellowBtn bg='coral'>버튼</YellowBtn>
+      {/* <YellowBtn bg='skyblue'>버튼</YellowBtn> */}
+      {/* <YellowBtn bg='coral'>버튼</YellowBtn> */}
+
+      {alert == true ? (
+        <div className='alert alert-warning'>2초 이내 구매시 할인</div>
+      ) : null}
+      {count}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
       <div className='row'>
         <div className='col-md-6'>
           <img
